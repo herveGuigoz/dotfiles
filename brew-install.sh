73ab273ed7d378ffd1014e45c83a3ae5a8cb993a
Guigoz Herve <43623121+herveGuigoz@users.noreply.github.com>
2020-11-04 18:14:19 +0100

    Update brew-install.sh

--- brew-install.sh
+++ brew-install.sh
@@ -12,7 +12,28 @@ brew install git
 echo "Install terminal stuff"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 brew cask install iterm2
-sed -i -e 's/plugins=(git)/plugins=(git flutter alias-finder docker)/g' ~/.zshrc
+brew install zsh-autosuggestions
+sed -i -e 's/plugins=(git)/plugins=(git zsh-autosuggestions flutter alias-finder docker)/g' ~/.zshrc
+
+# Aliases
+echo '# zsh' >>~/.zshrc
+echo "alias zshconfig='vim ~/.zshrc'" >>~/.zshrc
+echo "alias sourcezsh='source ~/.zshrc'" >>~/.zshrc
+echo "# Common" >>~/.zshrc
+echo "alias c='clear'" >>~/.zshrc
+echo "# Git" >>~/.zshrc
+echo "alias glh='git pull origin HEAD'" >>~/.zshrc
+echo '# Prevent common error' >>~/.zshrc
+echo "alias gti='git'" >>~/.zshrc
+echo "# Docker compose" >>~/.zshrc
+echo "alias dc='docker-compose'" >>~/.zshrc
+echo "alias dcrmc='docker-compose exec php bin/console c:c'" >>~/.zshrc
+echo "alias rmc='rm -rf var/cache'" >>~/.zshrc
+echo '# Aliase' >>~/.zshrc
+echo "alias al='alias'" >>~/.zshrc
+echo "alias af='alias-finder'" >>~/.zshrc
+
+source ~/.zshrc
 
 # Dev tools
 brew install node
