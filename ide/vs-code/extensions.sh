c4c4656766c95832bd6bb4ffe021ce1b95dd467f
Guigoz Herve <43623121+herveGuigoz@users.noreply.github.com>
2020-11-10 18:43:31 +0100

    Update extensions.sh

--- ide/vs-code/extensions.sh
+++ ide/vs-code/extensions.sh
@@ -3,3 +3,4 @@ code --install-extension Dart-Code.dart-code --force
 code --install-extension Dart-Code.flutter --force
 code --install-extension BendixMa.dart-data-class-generator --force
 code --install-extension octref.vetur --force
+code --install-extension gruntfuggly.todo-tree --force
