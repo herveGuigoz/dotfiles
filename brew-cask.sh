322589dad55ba35bc97805c6246dbc2b56c2ca59
herveGuigoz <herve.guigoz@gmail.com>
2020-11-23 15:17:34 +0100

    refactor: update sync script

--- brew-cask.sh
+++ brew-cask.sh
@@ -1,38 +1,38 @@
-echo "Install home brew"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+#!/bin/bash
 
-echo "Install brew cask"
+################################################
+# Install native apps                          #
+################################################
+
 brew tap homebrew/cask
 
-# Git
-echo "Install git"
+# dev
 brew install git
 
-# Terminal
-echo "Install terminal stuff"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 brew cask install iterm2
 brew install zsh-autosuggestions
 sed -i -e 's/plugins=(git)/plugins=(git zsh-autosuggestions flutter alias-finder docker)/g' ~/.zshrc
 
-# Dev tools
-brew install node
-brew install yarn
-brew install composer
 brew cask install docker
 brew tap dart-lang/dart
 brew install dart
 pub global activate fvm
+brew install node
+brew install yarn
+brew install composer
 brew cask install android-platform-tools
-
 brew cask install android-studio
 brew cask install visual-studio-code
 brew cask install insomnia
 brew install lcov
 
+# browsers
+brew cask install brave-browser
+
 # Misc
 brew install scrcpy
-brew cask install brave-browser
+brew cask install spotify
 brew cask install keepassxc
 brew cask install appcleaner
 brew cask install windscribe
@@ -40,5 +40,6 @@ brew cask install lulu
 brew cask install inkscape
 brew cask install discord
 brew install wifi-password
-# brew cask install sloth # https://github.com/sveinbjornt/Sloth
 
+# Remove outdated versions from the cellar
+brew cleanup
--- sync.sh
+++ sync.sh
@@ -1,39 +1,179 @@
 #!/bin/bash
-cd "$(dirname "$0")" || exit 5
-git pull
 
-function doIt() {
-  chmod +x ~/dotfiles/git/commit-msg
-  
-  if [ ! -d ~/code ]; then
-    mkdir ~/code
-  fi
-  if [ ! -f ~/.gitconfig ]; then
-    cp .gitconfig ~/.gitconfig
-  fi
-  
-  # Visual Studio Code
-  if [ -f ./Library/Application\ Support/Code/User/extensions.txt ]; then
-    extensions=$(cat ./Library/Application\ Support/Code/User/extensions.txt)
-    if [ ! -d /mnt/c ]; then
-      if which code >/dev/null 2>&1; then
-        for ext in $extensions; do
-          code --install-extension "$ext"
-        done
-      fi
-    fi
-  fi
+################################################
+# UTILS                                        #
+################################################
+
+answer_is_yes() {
+    [[ "$REPLY" =~ ^[Yy]$ ]] \
+        && return 0 \
+        || return 1
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  doIt
-else
-  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-  echo
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    doIt
-  fi
-fi
+ask() {
+    print_question "$1"
+    read
+}
 
-unset doIt
+ask_for_confirmation() {
+    print_question "$1 (y/n) "
+    read -n 1
+    printf "\n"
+}
+
+ask_for_sudo() {
+
+    # Ask for the administrator password upfront
+    sudo -v
+
+    # Update existing `sudo` time stamp until this script has finished
+    # https://gist.github.com/cowboy/3118588
+    while true; do
+        sudo -n true
+        sleep 60
+        kill -0 "$$" || exit
+    done &> /dev/null &
+
+}
+
+cmd_exists() {
+    [ -x "$(command -v "$1")" ] \
+        && printf 0 \
+        || printf 1
+}
+
+execute() {
+    $1 &> /dev/null
+    print_result $? "${2:-$1}"
+}
+
+get_answer() {
+    printf "$REPLY"
+}
+
+get_os() {
+
+    declare -r OS_NAME="$(uname -s)"
+    local os=""
+
+    if [ "$OS_NAME" == "Darwin" ]; then
+        os="osx"
+    elif [ "$OS_NAME" == "Linux" ] && [ -e "/etc/lsb-release" ]; then
+        os="ubuntu"
+    fi
+
+    printf "%s" "$os"
+
+}
+
+is_git_repository() {
+    [ "$(git rev-parse &>/dev/null; printf $?)" -eq 0 ] \
+        && return 0 \
+        || return 1
+}
+
+mkd() {
+    if [ -n "$1" ]; then
+        if [ -e "$1" ]; then
+            if [ ! -d "$1" ]; then
+                print_error "$1 - a file with the same name already exists!"
+            else
+                print_success "$1"
+            fi
+        else
+            execute "mkdir -p $1" "$1"
+        fi
+    fi
+}
+
+print_error() {
+    # Print output in red
+    printf "\e[0;31m  [✖] $1 $2\e[0m\n"
+}
+
+print_info() {
+    # Print output in purple
+    printf "\n\e[0;35m $1\e[0m\n\n"
+}
+
+print_question() {
+    # Print output in yellow
+    printf "\e[0;33m  [?] $1\e[0m"
+}
+
+print_result() {
+    [ $1 -eq 0 ] \
+        && print_success "$2" \
+        || print_error "$2"
+
+    [ "$3" == "true" ] && [ $1 -ne 0 ] \
+        && exit
+}
+
+print_success() {
+    # Print output in green
+    printf "\e[0;32m  [✔] $1\e[0m\n"
+}
+
+################################################
+# MAIN                                         #
+################################################
+
+main() {
+    # Homebrew
+    if ! command -v brew >/dev/null 2>&1; then
+        print_info "Installing Brew..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+        print_success "Brew installed."
+        ask_for_confirmation "do you want to cask all package from brew-cask.sh ?"
+        if answer_is_yes; then
+            print_info "Installing app..."
+            sh ./brew-cask.sh
+        fi
+    else
+        print_success "Brew already installed."
+        ask_for_confirmation "do you want to update brew packages ?"
+        if answer_is_yes; then
+            print_info "Updating Brew..."
+            brew update
+            print_info "Upgrading Packages..."
+            brew upgrade
+            print_info "Removing outdated versions from the cellar"
+            brew cleanup
+            print_success "Brew is up to date."
+        fi
+    fi
+
+    # Git
+    if [ -f ./git/.gitconfig ]; then
+        ask_for_confirmation "do you want to override .gitconfig ?"
+        if answer_is_yes; then
+            cp ./git/.gitconfig ~/.gitconfig
+        else
+            print_info 'gitconfig is omitted'
+        fi
+    else
+        print_error './git/.gitconfig file doesnt exist, gitconfig is omitted'
+    fi
+    if [ -f ./git/commit_types ]; then
+        chmod -x ./git/commit_types
+    fi
+
+    # Vs Code Extensions
+    if [ -f ./vs-code-extensions.txt ]; then
+        ask_for_confirmation "do you want to install vs code extensions ?"
+        if answer_is_yes; then
+            extensions=$(cat ./vs-code-extensions.txt)
+            for ext in $extensions; do
+                code --install-extension "$ext" --force
+            done
+        else
+            print_info 'vs code extensions is omitted'
+        fi
+    else
+        print_error './ide/vs-code/extensions.txt file doesnt exist, vs code extensions is omitted'
+    fi
+}
+
+main
 source ~/.bash_profile
